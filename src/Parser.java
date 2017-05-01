1f9fa52a42a31a63058bbcbc5adb56c2f0e87b3c
coolcom200 <leonfattakhov@yahoo.com>
2017-04-30 22:05:56 -0400

    reads files in and properly handles duplicate names

--- src/Parser.java
+++ src/Parser.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Parser {
+    Map<String, String> NamesToNumber = new HashMap<>();
+    ArrayList<String[]> DuplicateNames = new ArrayList<>();
 
-    public static void main(String[] args) {
-
+    public Parser() {
         String csvFile = "nerd club.csv";
         String line = "";
         String cvsSplitBy = ",";
@@ -16,14 +21,51 @@ public class Parser {
 
                 // use comma as separator
                 String[] country = line.split(cvsSplitBy);
+                String[] nameNum = new String[2];
+                String name = country[9] + " " + country[11];
+                if (NamesToNumber.containsKey(name)) {
+                    nameNum[0] = name;
+                    nameNum[1] = country[5];
+                    String tempNum = NamesToNumber.get(name);
+                    NamesToNumber.remove(name);
+                    DuplicateNames.add(0, nameNum);
+                    DuplicateNames.add(0, new String[]{name, tempNum});
 
-                System.out.print("Country [code= " + country[6] +" "+ country[8] + " , name=" + country[5] + "]");
+                } else {
+                    NamesToNumber.put(name, country[5]);
+                }
 
             }
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void importNames() {
+        String csvNames = "";
+        String line = "";
+        String cvsSplitBy = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+                String[] Name = line.split(cvsSplitBy);
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Parser P = new Parser();
+        P.
+
+
+                System.out.println("DONE");
 
     }
 
